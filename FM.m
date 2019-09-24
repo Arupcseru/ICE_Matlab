62978dfe4303c19be8312703a388de80a07fe248
Arup Sarkar <37178898+Arupcseru@users.noreply.github.com>
2019-09-24 21:50:06 +0600

    Add files via upload

---
+++ FM.m
@@ -0,0 +1,29 @@
+clc
+clear;
+
+Am = input('Enter Amplitude (Message) = ');
+Ac = input('Enter Amplitude (Carrier) = ');
+fm = input('Enter Message frequency = ');
+fc = input('Enter Carrier frequency = ');
+m = input('Enter Modulation Index = ');%10
+t = 0:0.001:01; %upto 1000 samples
+msg = Am*sin(2*pi*fm*t);
+subplot(3,1,1); %plotting message signal
+plot(t,msg);
+xlabel('Time');
+ylabel('Amplitude');
+title('Message ');
+
+carrier = Ac*sin(2*pi*fc*t);
+subplot(3,1,2); %plotting carrier signal
+plot(t,carrier);
+xlabel('Time');
+ylabel('Amplitude');
+title('Carrier Signal');
+
+y = Ac*sin(2*pi*fc*t+m*sin(2*pi*fm*t));
+subplot(3,1,3);%plotting FM (Frequency Modulated) signal
+plot(t,y,'r');
+xlabel('Time');
+ylabel('Amplitude');
+title('FM Signal');
